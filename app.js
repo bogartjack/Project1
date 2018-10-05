b237e3b119487133a57a2a90da84cae82062a428
Jack Bogart <bogart.jack98@gmail.com>
2018-10-05 11:57:58 -0600

    Pretty website things

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const WIDTH = $(window).width();
-const HEIGHT = $(window).height();
-const BLOCKHEIGHT = 20;
+const WIDTH = $('#game').width();
+const HEIGHT = $('#game').height();
+const BLOCKHEIGHT = 8;
 const BLOCKSWIDE = WIDTH/(BLOCKHEIGHT+2)-1;
 const BLOCKSTALL = HEIGHT/(BLOCKHEIGHT+2)-1;
 let evolving = false;
+let liveCells = 0;
+let iterations = 0;
+let $liveCells = $('#liveCells');
+let $iterations = $('#iterations');
+
 
 class Block {
 	constructor(x,y){
@@ -18,7 +23,7 @@ class Block {
 			.css('background', '#F0F0F0')
 			.css('height', BLOCKHEIGHT+'px')
 			.css('width', BLOCKHEIGHT+'px')
-			.css('border', '1px solid red')
+			.css('border', '1px solid black')
 			.data('xIndex', this.xIndex)
 			.data('yIndex', this.yIndex);
 //			.text(this.xIndex + ',' + this.yIndex);
@@ -27,11 +32,18 @@ class Block {
 		$('#game').append(this.$block);
 	}
 	toggleState(){
-		if (!this.state) this.state = true;
-		else this.state = false;
+		if (!this.state){
+			this.state = true;
+			liveCells--;
+		}	
+		else {
+			this.state = false;
+			liveCells++;
+		}
+		updateStats();
 	}
 	changeColorBasedOnState(){
-		if(this.state) this.$block.css('background-color', '#000000');
+		if(this.state) this.$block.css('background-color', '#'+Math.floor(Math.random()*16777215).toString(16));
 		else this.$block.css('background', '#F0F0F0');
 	}
 }
@@ -49,14 +61,8 @@ const board = {
 			this.allBlocks.push(row);
 		}
 	},
-	updateBoard (){
-		for (let i=0; i<BLOCKSTALL; i++){
-			for (let j=0; j<BLOCKSWIDE; j++){
-				this.allBlocks[i][j].changeColorBasedOnState();
-			}
-		}
-	},
 	evolve (){
+		setTimeout(() => {
 		if (evolving){
 			for (let i =0; i<BLOCKSTALL;i++){
 				for (let j =0; j<BLOCKSWIDE; j++){
@@ -74,35 +80,69 @@ const board = {
 						this.allBlocks[i][j].neighbors++;}	
 					if((i<BLOCKSTALL-1) && this.allBlocks[i+1][j].state){
 						this.allBlocks[i][j].neighbors++;}	
-					if((j<BLOCKSWIDE-1) && (i<BLOCKSTALL-1) && this.allBlocks[i+1][j+1].state){							this.allBlocks[i][j].neighbors++;}	
+					if((j<BLOCKSWIDE-1) && (i<BLOCKSTALL-1) && this.allBlocks[i+1][j+1].state){
+						this.allBlocks[i][j].neighbors++;}	
 				}
 			}	
 			for (let i=0; i<BLOCKSTALL; i++){
 				for (let j=0; j<BLOCKSWIDE; j++){
 					
 					if(this.allBlocks[i][j].neighbors < 2 && this.allBlocks[i][j].state){
-						this.allBlocks[i][j].state = false;}
+						this.allBlocks[i][j].state = false;
+						liveCells--;	
+					}
 					if(this.allBlocks[i][j].neighbors===2 && this.allBlocks[i][j].state) {
-						this.allBlocks[i][j].state = true;}
+						this.allBlocks[i][j].state = true;
+						liveCells++;
+					}
 					if(this.allBlocks[i][j].neighbors===3 && this.allBlocks[i][j].state) {
-						this.allBlocks[i][j].state = true;}
+						liveCells++;
+						this.allBlocks[i][j].state = true;
+					}
 					if(this.allBlocks[i][j].neighbors>3 && this.allBlocks[i][j].state){
-						this.allBlocks[i][j].state = false;}
+						liveCells--;
+						this.allBlocks[i][j].state = false;
+					}
 					if(this.allBlocks[i][j].neighbors===3 && !this.allBlocks[i][j].state){
-						this.allBlocks[i][j].state = true;}
+						liveCells++;
+						this.allBlocks[i][j].state = true;
+					}
 				}
 			}
 			for (let i=0; i<BLOCKSTALL; i++){
-				for (let j=0; j<BLOCKSWIDE; j++){
+			for (let j=0; j<BLOCKSWIDE; j++){
 					this.allBlocks[i][j].changeColorBasedOnState();
 					this.allBlocks[i][j].neighbors = 0;
 				}
 			}
+		iterations++;
 		}
+	}, 250);
 	}	
 };
 
 board.addBlocksToBoard();
+
+const overlay = () => {
+	$('#overlay').css('display', 'block');
+	$('#play').on('click', () => {
+		$('#overlay').css('display', 'none');
+	});
+
+}
+
+const updateStats = () =>{
+	$($iterations).text('number of iterations: '+iterations);
+	$($liveCells).text('number of live cells: '+liveCells);
+}
+
+$(document).ready( () => {
+	board.addBlocksToBoard();
+	$('#stats').prepend($liveCells);
+	$('#stats').prepend($iterations);
+	overlay();
+	updateStats();
+});
 
 $('.someBlock').mouseover((e) => {
 	if(e.buttons == 1){
@@ -117,8 +157,11 @@ $('body').keydown( (e) => {
 	if (evolving === false) {evolving = true;}
 	else {evolving = false;}
 
-	if (e.keyCode === 32){
-		
+	if (e.keyCode === 32){	
 		board.evolve();
+		updateStats();
+	}
+	else if(e.keyCode === 77){
+		overlay();
 	}
 });

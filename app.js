002d0d261d041204beaefb8554e6155d7593eba5
Jack Bogart <bogart.jack98@gmail.com>
2018-10-05 19:13:19 -0600

    Formatting and Rave

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const WIDTH = $('#game').width();
-const HEIGHT = $('#game').height();
-const BLOCKHEIGHT = 20;
-const BLOCKSWIDE = (WIDTH-WIDTH%BLOCKHEIGHT)/(BLOCKHEIGHT+2)-1;
-const BLOCKSTALL = (HEIGHT-HEIGHT%BLOCKHEIGHT)/(BLOCKHEIGHT+2)-1;
-let evolving = false;
+const WIDTH = Math.floor($('#content').width());
+const HEIGHT = Math.floor($('#content').height());
+console.log(WIDTH + '' +HEIGHT);
+const BLOCKHEIGHT = 8;
+const BLOCKSWIDE = Math.floor((WIDTH-(WIDTH%BLOCKHEIGHT))/(BLOCKHEIGHT+2))-1;
+console.log(BLOCKSWIDE);
+const BLOCKSTALL = Math.floor((HEIGHT-(HEIGHT%BLOCKHEIGHT))/(BLOCKHEIGHT+2))-1;
+console.log(BLOCKSTALL);
+
+$('#game').width((BLOCKHEIGHT+2)*BLOCKSWIDE);
+$('#game').height((BLOCKHEIGHT+2)*BLOCKSTALL);
+
 let liveCells = 0;
 let iterations = 0;
 let $liveCells = $('#liveCells');
 let $iterations = $('#iterations');
-
 
 class Block {
 	constructor(x,y){
@@ -25,8 +30,8 @@ class Block {
 			.css('width', BLOCKHEIGHT+'px')
 			.css('border', '1px solid black')
 			.data('xIndex', this.xIndex)
-			.data('yIndex', this.yIndex)
-			.text(this.xIndex + ',' + this.yIndex);
+			.data('yIndex', this.yIndex);
+//			.text(this.xIndex + ',' + this.yIndex);
 	}
 	addToPage(){
 		$('#game').append(this.$block);
@@ -34,11 +39,11 @@ class Block {
 	toggleState(){
 		if (!this.state){
 			this.state = true;
-			liveCells--;
+			liveCells++;
 		}	
 		else {
 			this.state = false;
-			liveCells++;
+			liveCells--;
 		}
 		updateStats();
 	}
@@ -63,7 +68,6 @@ const board = {
 	},
 	evolve (){
 		setTimeout(() => {
-		if (evolving){
 			for (let i =0; i<BLOCKSTALL;i++){
 				for (let j =0; j<BLOCKSWIDE; j++){
 					if((i>0) && (j>0) && this.allBlocks[i-1][j-1].state){
@@ -89,37 +93,119 @@ const board = {
 					
 					if(this.allBlocks[i][j].neighbors < 2 && this.allBlocks[i][j].state){
 						this.allBlocks[i][j].state = false;
-						liveCells--;	
+//						liveCells--;	
 					}
 					if(this.allBlocks[i][j].neighbors===2 && this.allBlocks[i][j].state) {
 						this.allBlocks[i][j].state = true;
-						liveCells++;
+//						liveCells++;
 					}
 					if(this.allBlocks[i][j].neighbors===3 && this.allBlocks[i][j].state) {
-						liveCells++;
+//						liveCells++;
 						this.allBlocks[i][j].state = true;
 					}
 					if(this.allBlocks[i][j].neighbors>3 && this.allBlocks[i][j].state){
-						liveCells--;
+//						liveCells--;
 						this.allBlocks[i][j].state = false;
 					}
 					if(this.allBlocks[i][j].neighbors===3 && !this.allBlocks[i][j].state){
-						liveCells++;
+//						liveCells++;
 						this.allBlocks[i][j].state = true;
 					}
 				}
 			}
+			liveCells=0;
 			for (let i=0; i<BLOCKSTALL; i++){
 			for (let j=0; j<BLOCKSWIDE; j++){
+					if(this.allBlocks[i][j].state) liveCells++;
 					this.allBlocks[i][j].changeColorBasedOnState();
 					this.allBlocks[i][j].neighbors = 0;
 				}
 			}
 		iterations++;
-		}
-	}, 250);
+		}, 250);
 	}	
 };
+const startTheRave = () => {
+	let temp = 0;
+	let up = true;
+	window.setInterval( () => {	
+		$('#game').css('border-color', '#'+Math.floor(Math.random()*16777215).toString(16));
+		$('.someBlock').css('border-color', '#'+Math.floor(Math.random()*1677215).toString(16));
+		$('#content').css('border-color', '#'+Math.floor(Math.random()*16777215).toString(16));
+		$('#stats').css('color', '#'+Math.floor(Math.random()*16777215).toString(16));
+		switch (temp){
+			case 0:
+				$('#game').css('border-thickness', '2px');
+				if (up){
+					$('#game').css('border-style', 'solid');
+					temp++;
+				}
+				else{
+					$('#game').css('border-style', 'none');
+					up = true;
+				}
+				
+				break;
+			case 1:
+				$('#game').css('border-thickness', '4px');
+				if (up){
+					$('#game').css('border-style', 'dotted');
+
+					temp++;
+				}
+				else{
+					$('#game').css('border-style', 'dashed inset ridge dotted');
+					temp--;
+				}
+				break;
+			case 2:
+				$('#game').css('border-thickness', '6px');
+				if (up){
+					$('#game').css('border-style', 'dashed');
+					temp++;
+				}
+				else{
+					$('#game').css('border-style', 'dotted solid double groove');
+					temp--;
+				}
+				break;
+			case 3:
+				$('#game').css('border-thickness', '8px');
+				if (up){
+					$('#game').css('border-style', 'double');
+					temp++;
+				}
+				else{
+					$('#game').css('border-style', 'outset');
+					temp--;
+				}
+				break;
+			case 4:
+				$('#game').css('border-thickness', '10px');
+				if (up){
+					$('#game').css('border-style', 'solid');
+					temp++;
+				}
+				else{
+					$('#game').css('border-style', 'inset');
+					temp--;
+				}
+			case 5:
+				$('#game').css('border-thickness', '12px');
+				if (up){
+					$('#game').css('border-style', 'groove');
+					up = false;
+			
+				}
+				else{
+					$('#game').css('border-style', 'ridge');
+					temp--;
+				}
+				break;
+		};
+
+	}, 250);
+}
 
 const overlay = () => {
 	$('#overlay').css('display', 'block');
@@ -140,10 +226,11 @@ $(document).ready( () => {
 	$('#stats').prepend($iterations);
 	drawOnBoard();
 	overlay();
+	startTheRave();
 	updateStats();
 });
 const drawOnBoard = () => {
-	$('.someBlock').mouseover((e) => {
+	$('.someBlock').mouseover((e) => { 
 		console.log('doohick');
 		if(e.buttons == 1){
 			console.log($(e.currentTarget).attr('id'));
@@ -155,8 +242,6 @@ const drawOnBoard = () => {
 }
 
 $('body').keydown( (e) => {
-	if (!evolving) {evolving = true;}
-	else {evolving = false;}
 	if (e.keyCode === 32){	
 		e.preventDefault();
 		board.evolve();
